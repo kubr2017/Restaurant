814a1518a9ae1bd8c7d5146379b6753ff4a1c162
kubr2017 <kubr2017@gmail.com>
2018-12-01 11:07:25 -0500

    added cache install strings

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,16 @@
+//install cache
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/'
+      ]);
+    })
+  );
+});
+
+//Fetch to requests
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(resp) {
