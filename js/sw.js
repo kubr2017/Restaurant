67fe9beb654e45aba77924c732e58b865959584f
kubr2017 <kubr2017@gmail.com>
2018-12-01 10:59:42 -0500

    added image no-reception-signal

--- js/sw.js
+++ js/sw.js
@@ -9,7 +9,7 @@ self.addEventListener('fetch', function(event) {
         return response;
       });
     }).catch(function() {
-      return caches.match('/sw-test/gallery/myLittleVader.jpg');
+      return caches.match('/img/No-reception-signal.jpg');
     })
   );
 });
