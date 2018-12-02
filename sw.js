8e3966936764666ff5f7798f34c337b5ca0606bd
kubr2017 <kubr2017@gmail.com>
2018-12-02 01:21:22 -0500

    123

--- sw.js
+++ sw.js
@@ -14,14 +14,18 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(resp) {
+      console.log(`resp=${resp.clone()}`);
       return resp || fetch(event.request).then(function(response) {
+        console.log(`go to fetch event.req=${event.request}`);
         let responseClone = response.clone();
         caches.open('v1').then(function(cache) {
+          console.log(`v1 opened`);
           cache.put(event.request, responseClone);
         });
         return response;
       });
     }).catch(function() {
+      console.log(`error hapened`);
       return caches.match('/img/No-reception-signal.jpg');
     })
   );
