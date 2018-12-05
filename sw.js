691661076fe693878c15486e1dcd00c83755380e
kubr2017 <kubr2017@gmail.com>
2018-12-05 14:27:40 -0500

    changed name of cache

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('v1').then(function(cache) {
+    caches.open('version-developers.google.com').then(function(cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
