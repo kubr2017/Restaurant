5cd4fd5199aace259ac0e1e4c09e2e07836019e4
kubr2017 <kubr2017@gmail.com>
2018-12-08 19:36:55 -0500

    added aviding POST method

--- sw.js
+++ sw.js
@@ -11,41 +11,46 @@ self.addEventListener('install', function(event) {
 
 //Fetch to requests
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(resp) {
-//       // console.log(`resp=${resp.clone()}`);
-//       return resp || fetch(event.request).then(function(response) {
-//         console.log(`go to fetch event.req=${event.request}`);
-//         let responseClone = response.clone();
-//         caches.open('v1').then(function(cache) {
-//           console.log(`v1 opened`);
-//           cache.put(event.request, responseClone);
-//         });
-//         return response;
-//       });
-//     }).catch(function() {
-//       console.log(`error hapened`);
-//       return caches.match('/img/No-reception-signal.jpg');
-//     })
-//   );
-// });
-
-
-// Code from developer.google.com
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('v1').then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
+    caches.match(event.request).then(function(resp) {
+      // console.log(`resp=${resp.clone()}`);
+      return resp || (if (!resp.status===202) {
+        console.log(`resp.status = ${resp.status}`);
+        fetch(event.request).then(function(response) {
+          console.log(`go to fetch event.req=${event.request}`);
+          let responseClone = response.clone();
+          caches.open('v1').then(function(cache) {
+            console.log(`v1 opened`);
+            cache.put(event.request, responseClone);
+          });
+        return response;
+        })
+      }else{
+        console.log(`Impossible get request from network and cache too`);
       });
+    }).catch(function() {
+      console.log(`error hapened`);
+      return caches.match('/img/No-reception-signal.jpg');
     })
   );
 });
+
+
+// Code from developer.google.com
+//
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.open('v1').then(function(cache) {
+//       return cache.match(event.request).then(function (response) {
+//         return response || fetch(event.request).then(function(response) {
+//           cache.put(event.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
 
 
 
