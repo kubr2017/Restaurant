6b6c1ec7d3d418a438497cb17b22f608e2b89681
kubr2017 <kubr2017@gmail.com>
2018-12-08 19:11:27 -0500

    changed name of cache to "v1"

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //install cache
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('version-developers.google.com').then(function(cache) {
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/'
       ]);
@@ -31,9 +31,12 @@ self.addEventListener('install', function(event) {
 //   );
 // });
 
+
+// Code from developer.google.com
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('version-developers.google.com').then(function(cache) {
+    caches.open('v1').then(function(cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
